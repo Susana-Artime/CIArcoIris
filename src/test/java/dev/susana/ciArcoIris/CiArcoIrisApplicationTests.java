83e73ad095f2c76f4f7ca997417b0096b4878d42
SusanaArtime <susartime@gmail.com>
2024-12-01 23:41:27 +0100

    add dependency in pom, configuration in application.properties and configuration in .gitignore

---
+++ src/test/java/dev/susana/ciArcoIris/CiArcoIrisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.susana.ciArcoIris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiArcoIrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
