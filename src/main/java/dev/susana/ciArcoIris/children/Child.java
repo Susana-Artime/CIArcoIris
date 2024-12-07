b7ceb8557f29754345f6b8972221a2a7d0dbc02f
SusanaArtime <susartime@gmail.com>
2024-12-07 06:59:11 +0100

    add model Child

---
+++ src/main/java/dev/susana/ciArcoIris/children/Child.java
@@ -0,0 +1,48 @@
+package dev.susana.ciArcoIris.children;
+
+import dev.susana.ciArcoIris.users.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.Date;
+
+import dev.susana.ciArcoIris.classrooms.Classroom;
+import dev.susana.ciArcoIris.guardians.Guardian;
+import lombok.*;
+    
+    @Entity
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Table(name = "children")
+    public class Child {
+    
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+    
+        @ManyToOne
+        @JoinColumn(name = "id_classroom", nullable = false)
+        private Classroom classroom;
+
+        @OneToMany
+        @JoinColumn(name = "id_guardian", nullable = false)
+        private Guardian guardian;
+    
+        @Column(nullable = false)
+        private String name;
+    
+        @Column(nullable = false)
+        private Date dayBirth;
+
+        private String comments;
+    
+      
+    
+
+    
+}
