fda71bc08ebd0096aaa2b15a9df4cd4f6f97e1ec
SusanaArtime <susartime@gmail.com>
2024-12-11 22:37:45 +0100

    add method findclassroombyname

--- src/main/java/dev/susana/ciArcoIris/classrooms/ClassroomRepository.java
+++ src/main/java/dev/susana/ciArcoIris/classrooms/ClassroomRepository.java
@@ -1,10 +1,12 @@
 package dev.susana.ciArcoIris.classrooms;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
     
+    Optional<Classroom> findByName(String name);
 }
 
