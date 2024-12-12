bef5f83fcae22582bbe60bce1f61bd11689d8eaf
SusanaArtime <susartime@gmail.com>
2024-12-12 02:38:50 +0100

    add method in GuardianRepository

--- src/main/java/dev/susana/ciArcoIris/guardians/GuardianRepository.java
+++ src/main/java/dev/susana/ciArcoIris/guardians/GuardianRepository.java
@@ -1,9 +1,12 @@
 package dev.susana.ciArcoIris.guardians;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GuardianRepository extends JpaRepository<Guardian, Long> {
-    
+
+    List<Guardian> findByChildId(Long childId);
 }
