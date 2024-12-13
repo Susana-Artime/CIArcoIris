4968e49ffe99f24f2c0d26661b3ed6568bcf5bb9
SusanaArtime <susartime@gmail.com>
2024-12-13 04:51:51 +0100

    restaured delete files - merge dev to main

---
+++ src/main/java/dev/susana/ciArcoIris/users/User.java
@@ -0,0 +1,33 @@
+package dev.susana.ciArcoIris.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String role;
+    
+}
